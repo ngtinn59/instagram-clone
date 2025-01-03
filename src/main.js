90edb52ffddfb9955a1646ccd76b1239541db802
ngtinn59 <ngtin590@gmail.com>
2025-01-03 22:01:24 +0700

    style: add Tailwind CSS and global styles
    - Import Tailwind CSS from "./assets/styles/tailwind.css"
    - Import global CSS styles from "./assets/styles/global.css"

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import "./assets/css/tailwind.css";
-import "./assets/css/global.css";
+import "./assets/styles/tailwind.css";
+import "./assets/styles/global.css";
 
 createApp(App).use(router).mount("#app");
