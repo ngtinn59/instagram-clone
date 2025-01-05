63682956ef0bfad0f674db5be92a3e49b01643cc
ngtinn59 <ngtin590@gmail.com>
2025-01-05 17:28:05 +0700

    feat: scroll stories

---
+++ src/components/FeedComponent.vue
@@ -0,0 +1,143 @@
+<template>
+  <main>
+    <section>
+      <div
+        class="flex space-x-2 p-4 border overflow-x-scroll scrollbar-none rounded-sm"
+      >
+        <div class="cursor-pointer">
+          <img
+            src="https://scontent.fsgn23-1.fna.fbcdn.net/v/t39.30808-6/276317378_3199421297046903_762311942076936829_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=a5f93a&_nc_eui2=AeFoMG0Gqo6t8PcxnnkzSEygKuADJqIEz4Uq4AMmogTPhTF3lK8zVKmjiDYx6MT5AS2tk6Qh8uRrunQEfutriLyJ&_nc_ohc=UOTjGvsrSVYQ7kNvgES5Abs&_nc_oc=Adg_49R-L1PgAL44Qjuicd88muQr_LscFujmE46Ng7jySLnqAPF3POXs1geSRYTKz1KC6i0TIR9q1xdnwPS0fOg3&_nc_zt=23&_nc_ht=scontent.fsgn23-1.fna&_nc_gid=AR_kJPHuSZ7tIsvyjMabeN7&oh=00_AYA5YAlfqSbi0gOAvD2cLgv9aO26HzUn0A7CHzG5aUd-sA&oe=677FE47D"
+            alt=""
+            class="h-14 w-14 rounded-full p-[1.5px] border-2 border-red-500 object-cover transition-transform duration-200 ease-out cursor-pointer hover:scale-110"
+          />
+          <p class="text-xs w-14 truncate text-center">ngtinn__</p>
+        </div>
+        <div class="cursor-pointer">
+          <img
+            src="https://scontent.fsgn23-1.fna.fbcdn.net/v/t39.30808-6/276317378_3199421297046903_762311942076936829_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=a5f93a&_nc_eui2=AeFoMG0Gqo6t8PcxnnkzSEygKuADJqIEz4Uq4AMmogTPhTF3lK8zVKmjiDYx6MT5AS2tk6Qh8uRrunQEfutriLyJ&_nc_ohc=UOTjGvsrSVYQ7kNvgES5Abs&_nc_oc=Adg_49R-L1PgAL44Qjuicd88muQr_LscFujmE46Ng7jySLnqAPF3POXs1geSRYTKz1KC6i0TIR9q1xdnwPS0fOg3&_nc_zt=23&_nc_ht=scontent.fsgn23-1.fna&_nc_gid=AR_kJPHuSZ7tIsvyjMabeN7&oh=00_AYA5YAlfqSbi0gOAvD2cLgv9aO26HzUn0A7CHzG5aUd-sA&oe=677FE47D"
+            alt=""
+            class="h-14 w-14 rounded-full p-[1.5px] border-2 border-red-500 object-cover transition-transform duration-200 ease-out cursor-pointer hover:scale-110"
+          />
+          <p class="text-xs w-14 truncate text-center">ngtinn__</p>
+        </div>
+        <div class="cursor-pointer">
+          <img
+            src="https://scontent.fsgn23-1.fna.fbcdn.net/v/t39.30808-6/276317378_3199421297046903_762311942076936829_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=a5f93a&_nc_eui2=AeFoMG0Gqo6t8PcxnnkzSEygKuADJqIEz4Uq4AMmogTPhTF3lK8zVKmjiDYx6MT5AS2tk6Qh8uRrunQEfutriLyJ&_nc_ohc=UOTjGvsrSVYQ7kNvgES5Abs&_nc_oc=Adg_49R-L1PgAL44Qjuicd88muQr_LscFujmE46Ng7jySLnqAPF3POXs1geSRYTKz1KC6i0TIR9q1xdnwPS0fOg3&_nc_zt=23&_nc_ht=scontent.fsgn23-1.fna&_nc_gid=AR_kJPHuSZ7tIsvyjMabeN7&oh=00_AYA5YAlfqSbi0gOAvD2cLgv9aO26HzUn0A7CHzG5aUd-sA&oe=677FE47D"
+            alt=""
+            class="h-14 w-14 rounded-full p-[1.5px] border-2 border-red-500 object-cover transition-transform duration-200 ease-out cursor-pointer hover:scale-110"
+          />
+          <p class="text-xs w-14 truncate text-center">ngtinn__</p>
+        </div>
+        <div class="cursor-pointer">
+          <img
+            src="https://scontent.fsgn23-1.fna.fbcdn.net/v/t39.30808-6/276317378_3199421297046903_762311942076936829_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=a5f93a&_nc_eui2=AeFoMG0Gqo6t8PcxnnkzSEygKuADJqIEz4Uq4AMmogTPhTF3lK8zVKmjiDYx6MT5AS2tk6Qh8uRrunQEfutriLyJ&_nc_ohc=UOTjGvsrSVYQ7kNvgES5Abs&_nc_oc=Adg_49R-L1PgAL44Qjuicd88muQr_LscFujmE46Ng7jySLnqAPF3POXs1geSRYTKz1KC6i0TIR9q1xdnwPS0fOg3&_nc_zt=23&_nc_ht=scontent.fsgn23-1.fna&_nc_gid=AR_kJPHuSZ7tIsvyjMabeN7&oh=00_AYA5YAlfqSbi0gOAvD2cLgv9aO26HzUn0A7CHzG5aUd-sA&oe=677FE47D"
+            alt=""
+            class="h-14 w-14 rounded-full p-[1.5px] border-2 border-red-500 object-cover transition-transform duration-200 ease-out cursor-pointer hover:scale-110"
+          />
+          <p class="text-xs w-14 truncate text-center">ngtinn__</p>
+        </div>
+        <div class="cursor-pointer">
+          <img
+            src="https://scontent.fsgn23-1.fna.fbcdn.net/v/t39.30808-6/276317378_3199421297046903_762311942076936829_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=a5f93a&_nc_eui2=AeFoMG0Gqo6t8PcxnnkzSEygKuADJqIEz4Uq4AMmogTPhTF3lK8zVKmjiDYx6MT5AS2tk6Qh8uRrunQEfutriLyJ&_nc_ohc=UOTjGvsrSVYQ7kNvgES5Abs&_nc_oc=Adg_49R-L1PgAL44Qjuicd88muQr_LscFujmE46Ng7jySLnqAPF3POXs1geSRYTKz1KC6i0TIR9q1xdnwPS0fOg3&_nc_zt=23&_nc_ht=scontent.fsgn23-1.fna&_nc_gid=AR_kJPHuSZ7tIsvyjMabeN7&oh=00_AYA5YAlfqSbi0gOAvD2cLgv9aO26HzUn0A7CHzG5aUd-sA&oe=677FE47D"
+            alt=""
+            class="h-14 w-14 rounded-full p-[1.5px] border-2 border-red-500 object-cover transition-transform duration-200 ease-out cursor-pointer hover:scale-110"
+          />
+          <p class="text-xs w-14 truncate text-center">ngtinn__</p>
+        </div>
+        <div class="cursor-pointer">
+          <img
+            src="https://scontent.fsgn23-1.fna.fbcdn.net/v/t39.30808-6/276317378_3199421297046903_762311942076936829_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=a5f93a&_nc_eui2=AeFoMG0Gqo6t8PcxnnkzSEygKuADJqIEz4Uq4AMmogTPhTF3lK8zVKmjiDYx6MT5AS2tk6Qh8uRrunQEfutriLyJ&_nc_ohc=UOTjGvsrSVYQ7kNvgES5Abs&_nc_oc=Adg_49R-L1PgAL44Qjuicd88muQr_LscFujmE46Ng7jySLnqAPF3POXs1geSRYTKz1KC6i0TIR9q1xdnwPS0fOg3&_nc_zt=23&_nc_ht=scontent.fsgn23-1.fna&_nc_gid=AR_kJPHuSZ7tIsvyjMabeN7&oh=00_AYA5YAlfqSbi0gOAvD2cLgv9aO26HzUn0A7CHzG5aUd-sA&oe=677FE47D"
+            alt=""
+            class="h-14 w-14 rounded-full p-[1.5px] border-2 border-red-500 object-cover transition-transform duration-200 ease-out cursor-pointer hover:scale-110"
+          />
+          <p class="text-xs w-14 truncate text-center">ngtinn__</p>
+        </div>
+        <div class="cursor-pointer">
+          <img
+            src="https://scontent.fsgn23-1.fna.fbcdn.net/v/t39.30808-6/276317378_3199421297046903_762311942076936829_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=a5f93a&_nc_eui2=AeFoMG0Gqo6t8PcxnnkzSEygKuADJqIEz4Uq4AMmogTPhTF3lK8zVKmjiDYx6MT5AS2tk6Qh8uRrunQEfutriLyJ&_nc_ohc=UOTjGvsrSVYQ7kNvgES5Abs&_nc_oc=Adg_49R-L1PgAL44Qjuicd88muQr_LscFujmE46Ng7jySLnqAPF3POXs1geSRYTKz1KC6i0TIR9q1xdnwPS0fOg3&_nc_zt=23&_nc_ht=scontent.fsgn23-1.fna&_nc_gid=AR_kJPHuSZ7tIsvyjMabeN7&oh=00_AYA5YAlfqSbi0gOAvD2cLgv9aO26HzUn0A7CHzG5aUd-sA&oe=677FE47D"
+            alt=""
+            class="h-14 w-14 rounded-full p-[1.5px] border-2 border-red-500 object-cover transition-transform duration-200 ease-out cursor-pointer hover:scale-110"
+          />
+          <p class="text-xs w-14 truncate text-center">ngtinn__</p>
+        </div>
+        <div class="cursor-pointer">
+          <img
+            src="https://scontent.fsgn23-1.fna.fbcdn.net/v/t39.30808-6/276317378_3199421297046903_762311942076936829_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=a5f93a&_nc_eui2=AeFoMG0Gqo6t8PcxnnkzSEygKuADJqIEz4Uq4AMmogTPhTF3lK8zVKmjiDYx6MT5AS2tk6Qh8uRrunQEfutriLyJ&_nc_ohc=UOTjGvsrSVYQ7kNvgES5Abs&_nc_oc=Adg_49R-L1PgAL44Qjuicd88muQr_LscFujmE46Ng7jySLnqAPF3POXs1geSRYTKz1KC6i0TIR9q1xdnwPS0fOg3&_nc_zt=23&_nc_ht=scontent.fsgn23-1.fna&_nc_gid=AR_kJPHuSZ7tIsvyjMabeN7&oh=00_AYA5YAlfqSbi0gOAvD2cLgv9aO26HzUn0A7CHzG5aUd-sA&oe=677FE47D"
+            alt=""
+            class="h-14 w-14 rounded-full p-[1.5px] border-2 border-red-500 object-cover transition-transform duration-200 ease-out cursor-pointer hover:scale-110"
+          />
+          <p class="text-xs w-14 truncate text-center">ngtinn__</p>
+        </div>
+        <div class="cursor-pointer">
+          <img
+            src="https://scontent.fsgn23-1.fna.fbcdn.net/v/t39.30808-6/276317378_3199421297046903_762311942076936829_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=a5f93a&_nc_eui2=AeFoMG0Gqo6t8PcxnnkzSEygKuADJqIEz4Uq4AMmogTPhTF3lK8zVKmjiDYx6MT5AS2tk6Qh8uRrunQEfutriLyJ&_nc_ohc=UOTjGvsrSVYQ7kNvgES5Abs&_nc_oc=Adg_49R-L1PgAL44Qjuicd88muQr_LscFujmE46Ng7jySLnqAPF3POXs1geSRYTKz1KC6i0TIR9q1xdnwPS0fOg3&_nc_zt=23&_nc_ht=scontent.fsgn23-1.fna&_nc_gid=AR_kJPHuSZ7tIsvyjMabeN7&oh=00_AYA5YAlfqSbi0gOAvD2cLgv9aO26HzUn0A7CHzG5aUd-sA&oe=677FE47D"
+            alt=""
+            class="h-14 w-14 rounded-full p-[1.5px] border-2 border-red-500 object-cover transition-transform duration-200 ease-out cursor-pointer hover:scale-110"
+          />
+          <p class="text-xs w-14 truncate text-center">ngtinn__</p>
+        </div>
+        <div class="cursor-pointer">
+          <img
+            src="https://scontent.fsgn23-1.fna.fbcdn.net/v/t39.30808-6/276317378_3199421297046903_762311942076936829_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=a5f93a&_nc_eui2=AeFoMG0Gqo6t8PcxnnkzSEygKuADJqIEz4Uq4AMmogTPhTF3lK8zVKmjiDYx6MT5AS2tk6Qh8uRrunQEfutriLyJ&_nc_ohc=UOTjGvsrSVYQ7kNvgES5Abs&_nc_oc=Adg_49R-L1PgAL44Qjuicd88muQr_LscFujmE46Ng7jySLnqAPF3POXs1geSRYTKz1KC6i0TIR9q1xdnwPS0fOg3&_nc_zt=23&_nc_ht=scontent.fsgn23-1.fna&_nc_gid=AR_kJPHuSZ7tIsvyjMabeN7&oh=00_AYA5YAlfqSbi0gOAvD2cLgv9aO26HzUn0A7CHzG5aUd-sA&oe=677FE47D"
+            alt=""
+            class="h-14 w-14 rounded-full p-[1.5px] border-2 border-red-500 object-cover transition-transform duration-200 ease-out cursor-pointer hover:scale-110"
+          />
+          <p class="text-xs w-14 truncate text-center">ngtinn__</p>
+        </div>
+        <div class="cursor-pointer">
+          <img
+            src="https://scontent.fsgn23-1.fna.fbcdn.net/v/t39.30808-6/276317378_3199421297046903_762311942076936829_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=a5f93a&_nc_eui2=AeFoMG0Gqo6t8PcxnnkzSEygKuADJqIEz4Uq4AMmogTPhTF3lK8zVKmjiDYx6MT5AS2tk6Qh8uRrunQEfutriLyJ&_nc_ohc=UOTjGvsrSVYQ7kNvgES5Abs&_nc_oc=Adg_49R-L1PgAL44Qjuicd88muQr_LscFujmE46Ng7jySLnqAPF3POXs1geSRYTKz1KC6i0TIR9q1xdnwPS0fOg3&_nc_zt=23&_nc_ht=scontent.fsgn23-1.fna&_nc_gid=AR_kJPHuSZ7tIsvyjMabeN7&oh=00_AYA5YAlfqSbi0gOAvD2cLgv9aO26HzUn0A7CHzG5aUd-sA&oe=677FE47D"
+            alt=""
+            class="h-14 w-14 rounded-full p-[1.5px] border-2 border-red-500 object-cover transition-transform duration-200 ease-out cursor-pointer hover:scale-110"
+          />
+          <p class="text-xs w-14 truncate text-center">ngtinn__</p>
+        </div>
+        <div class="cursor-pointer">
+          <img
+            src="https://scontent.fsgn23-1.fna.fbcdn.net/v/t39.30808-6/276317378_3199421297046903_762311942076936829_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=a5f93a&_nc_eui2=AeFoMG0Gqo6t8PcxnnkzSEygKuADJqIEz4Uq4AMmogTPhTF3lK8zVKmjiDYx6MT5AS2tk6Qh8uRrunQEfutriLyJ&_nc_ohc=UOTjGvsrSVYQ7kNvgES5Abs&_nc_oc=Adg_49R-L1PgAL44Qjuicd88muQr_LscFujmE46Ng7jySLnqAPF3POXs1geSRYTKz1KC6i0TIR9q1xdnwPS0fOg3&_nc_zt=23&_nc_ht=scontent.fsgn23-1.fna&_nc_gid=AR_kJPHuSZ7tIsvyjMabeN7&oh=00_AYA5YAlfqSbi0gOAvD2cLgv9aO26HzUn0A7CHzG5aUd-sA&oe=677FE47D"
+            alt=""
+            class="h-14 w-14 rounded-full p-[1.5px] border-2 border-red-500 object-cover transition-transform duration-200 ease-out cursor-pointer hover:scale-110"
+          />
+          <p class="text-xs w-14 truncate text-center">ngtinn__</p>
+        </div>
+        <div class="cursor-pointer">
+          <img
+            src="https://scontent.fsgn23-1.fna.fbcdn.net/v/t39.30808-6/276317378_3199421297046903_762311942076936829_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=a5f93a&_nc_eui2=AeFoMG0Gqo6t8PcxnnkzSEygKuADJqIEz4Uq4AMmogTPhTF3lK8zVKmjiDYx6MT5AS2tk6Qh8uRrunQEfutriLyJ&_nc_ohc=UOTjGvsrSVYQ7kNvgES5Abs&_nc_oc=Adg_49R-L1PgAL44Qjuicd88muQr_LscFujmE46Ng7jySLnqAPF3POXs1geSRYTKz1KC6i0TIR9q1xdnwPS0fOg3&_nc_zt=23&_nc_ht=scontent.fsgn23-1.fna&_nc_gid=AR_kJPHuSZ7tIsvyjMabeN7&oh=00_AYA5YAlfqSbi0gOAvD2cLgv9aO26HzUn0A7CHzG5aUd-sA&oe=677FE47D"
+            alt=""
+            class="h-14 w-14 rounded-full p-[1.5px] border-2 border-red-500 object-cover transition-transform duration-200 ease-out cursor-pointer hover:scale-110"
+          />
+          <p class="text-xs w-14 truncate text-center">ngtinn__</p>
+        </div>
+        <div class="cursor-pointer">
+          <img
+            src="https://scontent.fsgn23-1.fna.fbcdn.net/v/t39.30808-6/276317378_3199421297046903_762311942076936829_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=a5f93a&_nc_eui2=AeFoMG0Gqo6t8PcxnnkzSEygKuADJqIEz4Uq4AMmogTPhTF3lK8zVKmjiDYx6MT5AS2tk6Qh8uRrunQEfutriLyJ&_nc_ohc=UOTjGvsrSVYQ7kNvgES5Abs&_nc_oc=Adg_49R-L1PgAL44Qjuicd88muQr_LscFujmE46Ng7jySLnqAPF3POXs1geSRYTKz1KC6i0TIR9q1xdnwPS0fOg3&_nc_zt=23&_nc_ht=scontent.fsgn23-1.fna&_nc_gid=AR_kJPHuSZ7tIsvyjMabeN7&oh=00_AYA5YAlfqSbi0gOAvD2cLgv9aO26HzUn0A7CHzG5aUd-sA&oe=677FE47D"
+            alt=""
+            class="h-14 w-14 rounded-full p-[1.5px] border-2 border-red-500 object-cover transition-transform duration-200 ease-out cursor-pointer hover:scale-110"
+          />
+          <p class="text-xs w-14 truncate text-center">ngtinn__</p>
+        </div>
+        <div class="cursor-pointer">
+          <img
+            src="https://scontent.fsgn23-1.fna.fbcdn.net/v/t39.30808-6/276317378_3199421297046903_762311942076936829_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=a5f93a&_nc_eui2=AeFoMG0Gqo6t8PcxnnkzSEygKuADJqIEz4Uq4AMmogTPhTF3lK8zVKmjiDYx6MT5AS2tk6Qh8uRrunQEfutriLyJ&_nc_ohc=UOTjGvsrSVYQ7kNvgES5Abs&_nc_oc=Adg_49R-L1PgAL44Qjuicd88muQr_LscFujmE46Ng7jySLnqAPF3POXs1geSRYTKz1KC6i0TIR9q1xdnwPS0fOg3&_nc_zt=23&_nc_ht=scontent.fsgn23-1.fna&_nc_gid=AR_kJPHuSZ7tIsvyjMabeN7&oh=00_AYA5YAlfqSbi0gOAvD2cLgv9aO26HzUn0A7CHzG5aUd-sA&oe=677FE47D"
+            alt=""
+            class="h-14 w-14 rounded-full p-[1.5px] border-2 border-red-500 object-cover transition-transform duration-200 ease-out cursor-pointer hover:scale-110"
+          />
+          <p class="text-xs w-14 truncate text-center">ngtinn__</p>
+        </div>
+        <div class="cursor-pointer">
+          <img
+            src="https://scontent.fsgn23-1.fna.fbcdn.net/v/t39.30808-6/276317378_3199421297046903_762311942076936829_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=a5f93a&_nc_eui2=AeFoMG0Gqo6t8PcxnnkzSEygKuADJqIEz4Uq4AMmogTPhTF3lK8zVKmjiDYx6MT5AS2tk6Qh8uRrunQEfutriLyJ&_nc_ohc=UOTjGvsrSVYQ7kNvgES5Abs&_nc_oc=Adg_49R-L1PgAL44Qjuicd88muQr_LscFujmE46Ng7jySLnqAPF3POXs1geSRYTKz1KC6i0TIR9q1xdnwPS0fOg3&_nc_zt=23&_nc_ht=scontent.fsgn23-1.fna&_nc_gid=AR_kJPHuSZ7tIsvyjMabeN7&oh=00_AYA5YAlfqSbi0gOAvD2cLgv9aO26HzUn0A7CHzG5aUd-sA&oe=677FE47D"
+            alt=""
+            class="h-14 w-14 rounded-full p-[1.5px] border-2 border-red-500 object-cover transition-transform duration-200 ease-out cursor-pointer hover:scale-110"
+          />
+          <p class="text-xs w-14 truncate text-center">ngtinn__</p>
+        </div>
+      </div>
+    </section>
+  </main>
+</template>
+<script>
+export default {
+  name: "FeedComponent",
+};
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="home">
     <HelloWorld />
+    <Feed />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/Header.vue";
+import Feed from "@/components/FeedComponent.vue";
 
 export default {
   name: "HomeView",
   components: {
     HelloWorld,
+    Feed,
   },
 };
 </script>
