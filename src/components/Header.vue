b48ea23935aaead8e02a368b6c6ad6abedb1ab59
ngtinn59 <ngtin590@gmail.com>
2025-01-05 15:00:01 +0700

    Complete the header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -35,9 +35,44 @@
         <input
           class="pl-10 rounded-md focus:ring-black focus:border-black bg-gray-50 border-gray-500 text-sm"
           type="text"
+          placeholder="Search"
         />
       </div>
-      <h1 class="text-xl">Menu</h1>
+      <div class="flex space-x-4 items-center">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="size-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out hidden md:inline-flex"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+          />
+        </svg>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="size-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+          />
+        </svg>
+        <img
+          class="h-10 rounded-full cursor-pointer hover:scale-125"
+          src="https://scontent.fsgn23-1.fna.fbcdn.net/v/t39.30808-6/276317378_3199421297046903_762311942076936829_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=a5f93a&_nc_eui2=AeFoMG0Gqo6t8PcxnnkzSEygKuADJqIEz4Uq4AMmogTPhTF3lK8zVKmjiDYx6MT5AS2tk6Qh8uRrunQEfutriLyJ&_nc_ohc=UOTjGvsrSVYQ7kNvgES5Abs&_nc_oc=Adg_49R-L1PgAL44Qjuicd88muQr_LscFujmE46Ng7jySLnqAPF3POXs1geSRYTKz1KC6i0TIR9q1xdnwPS0fOg3&_nc_zt=23&_nc_ht=scontent.fsgn23-1.fna&_nc_gid=AR_kJPHuSZ7tIsvyjMabeN7&oh=00_AYA5YAlfqSbi0gOAvD2cLgv9aO26HzUn0A7CHzG5aUd-sA&oe=677FE47D"
+          alt=""
+        />
+      </div>
     </div>
   </div>
 </template>
